7ca3e63846119f055c2272bf27486c19d5221886
qianer1 <617017837@qq.com>
2022-09-24 21:28:47 +0800

    添加了字体图标并且添加了屏幕适配

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
  <h1>黑马头条</h1>
+ <div>
+  <div class="box">
+  <span class="toutiao toutiao-wenda"></span>
+  </div>
+  <van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+ </div>
     <router-view/>
   </div>
 </template>
@@ -14,5 +24,9 @@ data(){
 </script>
 
 <style lang="less">
-
+.box{
+  width: 200px;
+  height: 100px;
+  background-color: pink;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 import './styles/index.less'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
+//动态设置基准值
+import 'amfe-flexible'
+
+Vue.use(Vant);
 Vue.config.productionTip = false
 
 new Vue({
